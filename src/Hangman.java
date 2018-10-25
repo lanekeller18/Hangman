485e35a909d4dff970d444ff0f84c9cac4458b82
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-10-25 08:08:07 -0400

    Added some string variables

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Hangman {
+
+    public static void main(String[] args) {
+        Scanner keyboard;
+
+
+        String word;
+        String again;
+    }
 }
